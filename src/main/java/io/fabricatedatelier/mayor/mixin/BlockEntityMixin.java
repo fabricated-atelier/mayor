a13debe5f1ef17792f46bf577c82f26863c06b0a
JR1811 <janreichelt77@gmail.com>
2024-07-20 22:56:47 +0200

    added mouse scrolling (wip)

--- src/main/java/io/fabricatedatelier/mayor/mixin/BlockEntityMixin.java
+++ src/main/java/io/fabricatedatelier/mayor/mixin/BlockEntityMixin.java
@@ -6,10 +6,10 @@ import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(BlockEntity.class)
-public abstract class BlockEntityMixin implements CameraTarget {
+public class BlockEntityMixin implements CameraTarget {
 
     @Override
     public Vec3d mayor$getTargetPosition() {
-        return ((BlockEntity) (Object) this).getPos().toCenterPos().add(0, 1.0, 0);
+        return ((BlockEntity) (Object) this).getPos().toCenterPos().add(0, 0.5, 0);
     }
 }
