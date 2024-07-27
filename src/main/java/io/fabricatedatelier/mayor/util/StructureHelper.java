9c5f6d3dc70581da098be28ffc2fa3f4f575f0ab
Globox1997 <mitschke11@web.de>
2024-07-27 17:47:08 +0200

    tweaked structure helper

--- src/main/java/io/fabricatedatelier/mayor/util/StructureHelper.java
+++ src/main/java/io/fabricatedatelier/mayor/util/StructureHelper.java
@@ -1,9 +1,10 @@
 package io.fabricatedatelier.mayor.util;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.RegistryEntryLookup;
 import org.jetbrains.annotations.Nullable;
 
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
@@ -37,42 +38,43 @@ public class StructureHelper {
     public static Optional<StructureTemplate> getStructureTemplate(ServerWorld serverWorld, Identifier identifier) {
         StructureTemplateManager structureTemplateManager = serverWorld.getStructureTemplateManager();
         Optional<StructureTemplate> structure = structureTemplateManager.getTemplate(identifier);
-
         return structure;
-    };
+    }
+
+    public static Map<BlockPos, NbtCompound> getBlockMap(ServerWorld serverWorld, Identifier structureId, BlockRotation structureRotation) {
+        Map<BlockPos, NbtCompound> blockMap = new HashMap<BlockPos, NbtCompound>();
+        Optional<StructureTemplate> optional = StructureHelper.getStructureTemplate(serverWorld, structureId);
+        if (optional.isPresent() && optional.get() instanceof StructureTemplateAccess structureTemplateAccess) {
+            for (int i = 0; i < structureTemplateAccess.getBlockInfoLists().get(0).getAll().size(); i++) {
+                StructureBlockInfo structureBlockInfo = structureTemplateAccess.getBlockInfoLists().get(0).getAll().get(i);
+                if (structureBlockInfo.state().isAir()) {
+                    // maybe sync air too?
+                    continue;
+                }
+                BlockState blockState = structureBlockInfo.state().rotate(structureRotation);
+                if (structureBlockInfo.state().isOf(Blocks.JIGSAW)) {
+                    String string = structureBlockInfo.nbt().getString("final_state");
+                    try {
+                        blockState = BlockArgumentParser.block(serverWorld.createCommandRegistryWrapper(RegistryKeys.BLOCK), string, true).blockState();
+                    } catch (CommandSyntaxException var15) {
+                        Mayor.LOGGER.error("Error while parsing blockstate {} in jigsaw block @ {}", string, structureBlockInfo.pos());
+                    }
+                }
+                BlockPos pos = StructureTemplate.transformAround(structureBlockInfo.pos().up(), BlockMirror.NONE, structureRotation, BlockPos.ORIGIN);
+
+                blockMap.put(pos, NbtHelper.fromBlockState(blockState));
+            }
+        }
+        return blockMap;
+    }
 
     public static boolean updateMajorStructure(ServerPlayerEntity serverPlayerEntity, Identifier structureId, BlockRotation structureRotation) {
-        Optional<StructureTemplate> optional = StructureHelper.getStructureTemplate(serverPlayerEntity.getServerWorld(), structureId);
-        if (optional.isPresent() && optional.get() instanceof StructureTemplateAccess structureTemplateAccess) {
-
-            Map<BlockPos, NbtCompound> blockMap = new HashMap<BlockPos, NbtCompound>();
-            if (structureTemplateAccess.getBlockInfoLists().size() > 0) {
-
-                for (int i = 0; i < structureTemplateAccess.getBlockInfoLists().get(0).getAll().size(); i++) {
-                    StructureBlockInfo structureBlockInfo = structureTemplateAccess.getBlockInfoLists().get(0).getAll().get(i);
-                    if (structureBlockInfo.state().isAir()) {
-                        // maybe sync air too?
-                        continue;
-                    }
-                    BlockState blockState = structureBlockInfo.state().rotate(structureRotation);
-                    if (structureBlockInfo.state().isOf(Blocks.JIGSAW)) {
-                        String string = structureBlockInfo.nbt().getString("final_state");
-                        try {
-                            blockState = BlockArgumentParser.block(serverPlayerEntity.getServerWorld().createCommandRegistryWrapper(RegistryKeys.BLOCK), string, true).blockState();
-                        } catch (CommandSyntaxException var15) {
-                            Mayor.LOGGER.error("Error while parsing blockstate {} in jigsaw block @ {}", string, structureBlockInfo.pos());
-                        }
-                    }
-                    BlockPos pos = StructureTemplate.transformAround(structureBlockInfo.pos().up(), BlockMirror.NONE, structureRotation, BlockPos.ORIGIN);
-
-                    blockMap.put(pos, NbtHelper.fromBlockState(blockState));
-                }
-                MayorManager mayorManager = ((MayorManagerAccess) serverPlayerEntity).getMayorManager();
-                mayorManager.setStructureId(structureId);
-                mayorManager.setStructureRotation(structureRotation);
-                ServerPlayNetworking.send(serverPlayerEntity, new StructurePacket(structureId, blockMap, structureRotation));
-            }
-
+        Map<BlockPos, NbtCompound> blockMap = StructureHelper.getBlockMap(serverPlayerEntity.getServerWorld(), structureId, structureRotation);
+        if (!blockMap.isEmpty()) {
+            MayorManager mayorManager = ((MayorManagerAccess) serverPlayerEntity).getMayorManager();
+            mayorManager.setStructureId(structureId);
+            mayorManager.setStructureRotation(structureRotation);
+            ServerPlayNetworking.send(serverPlayerEntity, new StructurePacket(structureId, blockMap, structureRotation));
             return true;
         }
         return false;
@@ -89,34 +91,58 @@ public class StructureHelper {
     }
 
     public static BlockRotation getStructureRotation(int structureRotation) {
-        switch (structureRotation) {
-        case 0:
-            return BlockRotation.NONE;
-        case 1:
-            return BlockRotation.CLOCKWISE_90;
-        case 2:
-            return BlockRotation.CLOCKWISE_180;
-        case 3:
-            return BlockRotation.COUNTERCLOCKWISE_90;
-        default:
-            return BlockRotation.NONE;
-        }
+        return switch (structureRotation) {
+        case 0 -> BlockRotation.NONE;
+        case 1 -> BlockRotation.CLOCKWISE_90;
+        case 2 -> BlockRotation.CLOCKWISE_180;
+        case 3 -> BlockRotation.COUNTERCLOCKWISE_90;
+        default -> BlockRotation.NONE;
+        };
     }
 
     public static BlockRotation getRotatedStructureRotation(BlockRotation structureRotation, boolean rotateLeft) {
-        switch (structureRotation) {
-        case BlockRotation.NONE:
-            return rotateLeft ? BlockRotation.COUNTERCLOCKWISE_90 : BlockRotation.CLOCKWISE_90;
-        case BlockRotation.CLOCKWISE_90:
-            return rotateLeft ? BlockRotation.NONE : BlockRotation.CLOCKWISE_180;
-        case BlockRotation.CLOCKWISE_180:
-            return rotateLeft ? BlockRotation.CLOCKWISE_90 : BlockRotation.COUNTERCLOCKWISE_90;
-        case BlockRotation.COUNTERCLOCKWISE_90:
-            return rotateLeft ? BlockRotation.CLOCKWISE_180 : BlockRotation.NONE;
-        default:
-            return BlockRotation.NONE;
+        return switch (structureRotation) {
+        case BlockRotation.NONE -> rotateLeft ? BlockRotation.COUNTERCLOCKWISE_90 : BlockRotation.CLOCKWISE_90;
+        case BlockRotation.CLOCKWISE_90 -> rotateLeft ? BlockRotation.NONE : BlockRotation.CLOCKWISE_180;
+        case BlockRotation.CLOCKWISE_180 -> rotateLeft ? BlockRotation.CLOCKWISE_90 : BlockRotation.COUNTERCLOCKWISE_90;
+        case BlockRotation.COUNTERCLOCKWISE_90 -> rotateLeft ? BlockRotation.CLOCKWISE_180 : BlockRotation.NONE;
+        default -> BlockRotation.NONE;
+        };
+
+    }
+
+    public static List<ItemStack> getStructureItemRequirements(ServerWorld serverWorld, Identifier structureId) {
+        List<ItemStack> requiredItemStacks = new ArrayList<ItemStack>();
+        Map<BlockPos, NbtCompound> blockMap = StructureHelper.getBlockMap(serverWorld, structureId, BlockRotation.NONE);
+
+        RegistryEntryLookup<Block> blockLookup = serverWorld.createCommandRegistryWrapper(RegistryKeys.BLOCK);
+
+        Iterator<Map.Entry<BlockPos, NbtCompound>> iterator = blockMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<BlockPos, NbtCompound> entry = iterator.next();
+
+            ItemStack itemStack = new ItemStack(NbtHelper.toBlockState(blockLookup, entry.getValue()).getBlock().asItem());
+            if (requiredItemStacks.size() <= 0) {
+                requiredItemStacks.add(itemStack);
+            } else {
+                for (int i = 0; i < requiredItemStacks.size(); i++) {
+                    if (requiredItemStacks.get(i).isOf(itemStack.getItem())) {
+                        if (requiredItemStacks.get(i).getCount() >= requiredItemStacks.get(i).getMaxCount()) {
+                            continue;
+                        }
+                        itemStack.setCount(requiredItemStacks.get(i).getCount() + 1);
+                        requiredItemStacks.remove(i);
+                        requiredItemStacks.add(itemStack);
+                        break;
+                    }
+                    if (i == requiredItemStacks.size() - 1) {
+                        requiredItemStacks.add(itemStack);
+                    }
+                }
+            }
         }
 
+        return requiredItemStacks;
     }
 
 }
