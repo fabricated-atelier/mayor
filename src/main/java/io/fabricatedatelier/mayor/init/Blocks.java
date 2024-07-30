a541d5e59712ed6fce835b143a30a0bd20dae948
JR1811 <janreichelt77@gmail.com>
2024-07-30 02:49:50 +0200

    clean up manual block item registration

--- src/main/java/io/fabricatedatelier/mayor/init/Blocks.java
+++ src/main/java/io/fabricatedatelier/mayor/init/Blocks.java
@@ -4,6 +4,7 @@ import io.fabricatedatelier.mayor.Mayor;
 import io.fabricatedatelier.mayor.block.custom.CameraDebugBlock;
 import io.fabricatedatelier.mayor.block.custom.LumberStorageBlock;
 import io.fabricatedatelier.mayor.block.custom.StoneStorageBlock;
+import io.fabricatedatelier.mayor.item.StoneStorageBlockItem;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
@@ -19,26 +20,31 @@ import java.util.List;
 public class Blocks {
     public static final Block CAMERA_DEBUG = registerWithItem("camera_debug", new CameraDebugBlock(AbstractBlock.Settings.create()));
     public static final Block LUMBER_STORAGE = registerWithItem("lumber_storage", new LumberStorageBlock(AbstractBlock.Settings.create()));
-    public static final Block STONE_STORAGE = registerWithItem("stone_storage", new StoneStorageBlock(AbstractBlock.Settings.create()));
+    public static final Block STONE_STORAGE = register("stone_storage",
+            new StoneStorageBlock(AbstractBlock.Settings.create()),
+            new BlockItemData<>(
+                    new StoneStorageBlockItem(Blocks.STONE_STORAGE, new Item.Settings()),
+                    List.of(ItemGroups.MAYOR_ITEMS)
+            )
+    );
 
 
-    private static <T extends Block, U extends BlockItem> T register(String name, T block, @Nullable BlockItemData blockItemData) {
+    private static <T extends Block, U extends BlockItem> T register(String name, T block, @Nullable BlockItemData<U> blockItemData) {
         Registry.register(Registries.BLOCK, Mayor.identifierOf(name), block);
         if (blockItemData != null) {
-            BlockItem blockItem = new BlockItem(block, blockItemData.itemSettings());
-            Registry.register(Registries.ITEM, Mayor.identifierOf(name), blockItem);
+            Registry.register(Registries.ITEM, Mayor.identifierOf(name), blockItemData.blockItem);
             if (blockItemData.itemGroups() != null) {
-                Items.addToItemGroups(blockItem, blockItemData.itemGroups());
+                Items.addToItemGroups(blockItemData.blockItem, blockItemData.itemGroups());
             }
         }
         return block;
     }
 
     private static <T extends Block> T registerWithItem(String name, T block) {
-        return register(name, block, new BlockItemData(new Item.Settings(), List.of(ItemGroups.MAYOR_ITEMS)));
+        return register(name, block, new BlockItemData<>(new BlockItem(block, new Item.Settings()), List.of(ItemGroups.MAYOR_BLOCKS)));
     }
 
-    private record BlockItemData(Item.Settings itemSettings, @Nullable List<RegistryKey<ItemGroup>> itemGroups) {
+    private record BlockItemData<T extends BlockItem>(T blockItem, @Nullable List<RegistryKey<ItemGroup>> itemGroups) {
     }
 
     public static void initialize() {
