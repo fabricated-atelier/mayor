8910e504ca66504d3098591acae080b84213dfff
JR1811 <janreichelt77@gmail.com>
2024-08-02 19:26:14 +0200

    added blockentity content rendering

--- src/main/java/io/fabricatedatelier/mayor/MayorClient.java
+++ src/main/java/io/fabricatedatelier/mayor/MayorClient.java
@@ -1,11 +1,16 @@
 package io.fabricatedatelier.mayor;
 
+import io.fabricatedatelier.mayor.block.entity.client.LumberStorageBlockEntityRenderer;
+import io.fabricatedatelier.mayor.init.BlockEntities;
 import io.fabricatedatelier.mayor.init.KeyBindings;
 import io.fabricatedatelier.mayor.init.Renderer;
 import io.fabricatedatelier.mayor.network.CustomS2CNetworking;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
+import net.minecraft.client.render.entity.model.EntityModelLayer;
 
 @Environment(EnvType.CLIENT)
 public class MayorClient implements ClientModInitializer {
@@ -15,5 +20,7 @@ public class MayorClient implements ClientModInitializer {
         CustomS2CNetworking.initialize();
         Renderer.initialize();
         KeyBindings.initialize();
+
+        BlockEntityRendererFactories.register(BlockEntities.LUMBER_STORAGE, context -> new LumberStorageBlockEntityRenderer<>());
     }
 }
---
+++ src/main/java/io/fabricatedatelier/mayor/block/entity/client/LumberStorageBlockEntityRenderer.java
@@ -0,0 +1,59 @@
+package io.fabricatedatelier.mayor.block.entity.client;
+
+import io.fabricatedatelier.mayor.block.entity.LumberStorageBlockEntity;
+import io.fabricatedatelier.mayor.util.MayorProperties;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.RotationAxis;
+
+public class LumberStorageBlockEntityRenderer<T extends LumberStorageBlockEntity> implements BlockEntityRenderer<T> {
+    private int tick = 0, rotTick = 0;
+    @Override
+    public void render(T entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (!(client.world instanceof ClientWorld world)) return;
+
+        tick ++;
+        if (tick % 100 == 0) rotTick++;
+        if ( rotTick > 10) rotTick = 0;
+
+        BlockState state = entity.getCachedState();
+        switch (state.get(MayorProperties.SHAPE)) {
+            case ALL_WALLS -> {
+                int blockPerLog = 4;
+
+                matrices.push();
+                matrices.translate(0.2, 0.125, 0);
+                renderLog(blockPerLog, client, matrices, light, overlay, vertexConsumers, entity);
+                matrices.pop();
+            }
+        }
+    }
+
+    private void renderLog(int blockCount, MinecraftClient client, MatrixStack matrices,
+                                  int light, int overlay, VertexConsumerProvider vertexConsumers, T entity) {
+        float logScale = 1f / blockCount;
+        for (int i = 0; i < blockCount; i++) {
+            matrices.push();
+            matrices.scale(logScale, logScale, logScale);
+            matrices.translate(0.5, 0.5, 0.5);
+            matrices.translate(0, 0, i);
+            matrices.scale(2f, 2f, 2f);
+            matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(90));
+            client.getItemRenderer().renderItem(new ItemStack(Blocks.SPRUCE_LOG),
+                    ModelTransformationMode.FIXED, light, overlay,
+                    matrices, vertexConsumers,
+                    client.world, (int) entity.getPos().asLong());
+
+            matrices.pop();
+        }
+    }
+}
