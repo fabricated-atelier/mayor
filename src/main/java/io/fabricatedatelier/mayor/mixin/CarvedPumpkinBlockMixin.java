a7abb21d03f4713d6226843ffe7861bac289c353
Globox1997 <mitschke11@web.de>
2024-08-16 18:34:40 +0200

    iron golem spawn addition

---
+++ src/main/java/io/fabricatedatelier/mayor/mixin/CarvedPumpkinBlockMixin.java
@@ -0,0 +1,31 @@
+package io.fabricatedatelier.mayor.mixin;
+
+import io.fabricatedatelier.mayor.access.MayorVillageStateAccess;
+import io.fabricatedatelier.mayor.state.VillageData;
+import io.fabricatedatelier.mayor.util.MayorStateHelper;
+import net.minecraft.block.CarvedPumpkinBlock;
+import net.minecraft.block.pattern.BlockPattern;
+import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(CarvedPumpkinBlock.class)
+public class CarvedPumpkinBlockMixin {
+
+    @Inject(method = "trySpawnEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/CarvedPumpkinBlock;spawnEntity(Lnet/minecraft/world/World;Lnet/minecraft/block/pattern/BlockPattern$Result;Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/BlockPos;)V", ordinal = 1), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void trySpawnEntityMixin(World world, BlockPos pos, CallbackInfo info, BlockPattern.Result result, BlockPattern.Result result2, IronGolemEntity ironGolemEntity) {
+        if (world instanceof ServerWorld serverWorld) {
+            VillageData villageData = MayorStateHelper.getClosestVillage(serverWorld, pos);
+            if (villageData != null) {
+                villageData.getIronGolems().add(ironGolemEntity.getUuid());
+                ((MayorVillageStateAccess) serverWorld).getMayorVillageState().markDirty();
+            }
+        }
+    }
+}
