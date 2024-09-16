09000fcc924fdd77ee2b600aaed0d475b039b1aa
JR1811 <janreichelt77@gmail.com>
2024-09-16 16:14:40 +0200

    set storage mining tool to axes

--- src/main/java/io/fabricatedatelier/mayor/datagen/TagProvider.java
+++ src/main/java/io/fabricatedatelier/mayor/datagen/TagProvider.java
@@ -1,6 +1,7 @@
 package io.fabricatedatelier.mayor.datagen;
 
 import io.fabricatedatelier.mayor.Mayor;
+import io.fabricatedatelier.mayor.init.Blocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
@@ -20,7 +21,8 @@ public class TagProvider {
 
         @Override
         protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
-
+            getOrCreateTagBuilder(net.minecraft.registry.tag.BlockTags.AXE_MINEABLE)
+                    .add(Blocks.LUMBER_STORAGE, Blocks.STONE_STORAGE).setReplace(false);
         }
     }
 
