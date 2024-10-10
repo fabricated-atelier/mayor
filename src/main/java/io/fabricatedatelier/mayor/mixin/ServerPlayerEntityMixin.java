fb57407e936723ad8b2840d48aecee4cfa3369ee
Globox1997 <mitschke11@web.de>
2024-10-10 22:52:59 +0200

    added village title feature

--- src/main/java/io/fabricatedatelier/mayor/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/io/fabricatedatelier/mayor/mixin/ServerPlayerEntityMixin.java
@@ -1,13 +1,23 @@
 package io.fabricatedatelier.mayor.mixin;
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.mojang.authlib.GameProfile;
 import io.fabricatedatelier.mayor.access.ServerPlayerAccess;
+import io.fabricatedatelier.mayor.state.VillageData;
+import io.fabricatedatelier.mayor.util.StateHelper;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.packet.s2c.play.PositionFlag;
+import net.minecraft.network.packet.s2c.play.TitleS2CPacket;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,11 +26,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.Set;
 
 @Mixin(ServerPlayerEntity.class)
-public class ServerPlayerEntityMixin implements ServerPlayerAccess {
+public abstract class ServerPlayerEntityMixin extends PlayerEntity implements ServerPlayerAccess {
 
+    @Shadow
+    public abstract ServerWorld getServerWorld();
 
+    @Shadow
+    public ServerPlayNetworkHandler networkHandler;
+
+    @Nullable
+    @Unique
+    private BlockPos titleVillageCenter = null;
     @Unique
     private boolean wasInMayorView = false;
+
+    public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
+        super(world, pos, yaw, gameProfile);
+    }
 
     @Inject(method = "setCameraEntity", at = @At("TAIL"))
     private void setCameraEntityMixin(@Nullable Entity entity, CallbackInfo info) {
@@ -32,6 +54,19 @@ public class ServerPlayerEntityMixin implements ServerPlayerAccess {
     @WrapWithCondition(method = "setCameraEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;teleport(Lnet/minecraft/server/world/ServerWorld;DDDLjava/util/Set;FF)Z"))
     private boolean setCameraEntityMixin(ServerPlayerEntity instance, ServerWorld world, double destX, double destY, double destZ, Set<PositionFlag> flags, float yaw, float pitch) {
         return !this.wasInMayorView;
+    }
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void tickMixin(CallbackInfo info) {
+        VillageData villageData = StateHelper.getClosestVillage(this.getServerWorld(), this.getBlockPos());
+        if (villageData != null) {
+            if (this.titleVillageCenter == null || !this.titleVillageCenter.equals(villageData.getCenterPos())) {
+                this.networkHandler.sendPacket(new TitleS2CPacket(Text.translatable("mayor.village.name", villageData.getName())));
+                this.titleVillageCenter = villageData.getCenterPos();
+            }
+        } else if (this.titleVillageCenter != null) {
+            this.titleVillageCenter = null;
+        }
     }
 
     @Override
